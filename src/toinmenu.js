ab099eb0a9bf87590617c656d97c74e6e53b618a
kirito-gostozin <78399821+kirito-gostozin@users.noreply.github.com>
2021-02-02 01:46:20 -0300

    Add files via upload

---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do 𝑲𝑰𝑹𝑰𝑻𝑶 𝑶𝑭 𝑺𝑨𝑫ッ*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = toinmenu
